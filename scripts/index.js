46f7e0d46fc3a23553d4a414027d2619e25359e2
Dmitry Karyakin <web4t@yandex.ru>
2020-10-05 22:11:16 +0300

    fix, style, recoding: проработать первое ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,27 @@
 const buttonOpenPopup = document.querySelector(".profile__edit-button");
 const buttonClosePopup = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
+let nameInput = document.querySelector(".popup__text_type_name");
+let jobInput = document.querySelector(".popup__text_type_job");
+let nameReadet = document.querySelector(".profile__title"); 
+let jobReadet = document.querySelector(".profile__subtitle");
+let formElement = document.querySelector(".popup__container");
 
-const popupToggle = () => {
+const popupToggleClose = () => {
   popup.classList.toggle("popup_opened");
 }
 
-buttonOpenPopup.addEventListener("click", popupToggle);
-buttonClosePopup.addEventListener("click", popupToggle);
-let formElement = document.querySelector(".popup__container");
+const popupToggleOpen = () => {
+  popup.classList.toggle("popup_opened");
+  nameInput.value = nameReadet.textContent;
+  jobInput.value = jobReadet.textContent;
+}
+
+buttonOpenPopup.addEventListener("click", popupToggleOpen);
+buttonClosePopup.addEventListener("click", popupToggleClose);
 
 function formSubmitHandler (evt) {
  evt.preventDefault(); 
- let nameInput = document.querySelector(".popup__text_type_name");
- let jobInput = document.querySelector(".popup__text_type_job");
- let nameReadet = document.querySelector(".profile__title"); 
- let jobReadet = document.querySelector(".profile__subtitle");
  nameReadet.textContent = nameInput.value;
  jobReadet.textContent = jobInput.value;
  popupToggle();
