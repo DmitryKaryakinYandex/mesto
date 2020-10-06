b1a606cd5f88d4f760c4bb1d5531e30d4909686d
Dmitry Karyakin <web4t@yandex.ru>
2020-10-06 13:02:31 +0300

    style: убрать объявление шрифта у элемента

--- scripts/index.js
+++ scripts/index.js
@@ -24,7 +24,7 @@ function formSubmitHandler (evt) {
  evt.preventDefault(); 
  nameReadet.textContent = nameInput.value;
  jobReadet.textContent = jobInput.value;
- popupToggle();
+ popupToggleClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
