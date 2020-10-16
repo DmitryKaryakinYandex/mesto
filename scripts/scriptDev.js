24ed9f38eb84f1edb47e68490f68d31b5111496f
Dmitry Karyakin <web4t@yandex.ru>
2020-10-16 23:13:16 +0300

    feat,style: добавить закрашивание лайка, добавить всплытие попапа картинки, изменить стиль картинки карточки элемента

--- scripts/scriptDev.js
+++ scripts/scriptDev.js
@@ -13,6 +13,8 @@ const formElementAdd = document.querySelector('.popup-add__container');
 const stackName = document.querySelector('.popup-add__text_type_name');
 const stackLink = document.querySelector('.popup-add__text_type_link');
 const template = document.querySelector('.template');
+const popupImage = document.querySelector('.popup-images');
+const buttonClosePopupImage = document.querySelector('.popup-images__close');
 
 const buttonOpenPopupAdd = document.querySelector(".profile__add-button");
 const buttonClosePopupAdd = document.querySelector(".popup-add__close");
@@ -20,11 +22,11 @@ const popupAdd = document.querySelector(".popup-add");
 
 const popupAddToggleClose = () => {
     popupAdd.classList.toggle("popup-add_opened");
-  }
+  };
   
   const popupAddToggleOpen = () => {
     popupAdd.classList.toggle("popup-add_opened");
-  }
+  };
   
   buttonOpenPopupAdd.addEventListener("click", popupAddToggleOpen);
   buttonClosePopupAdd.addEventListener("click", popupAddToggleClose);
@@ -35,11 +37,23 @@ const getElement = (element) => {
     elementTemplate.querySelector('.elements__text').innerText = element.name;
     elementTemplate.querySelector('.elements__image').setAttribute('src',element.link);
     elementTemplate.querySelector('.elements__image').setAttribute('alt',element.name);
+    
+    const buttonTouchLike = elementTemplate.querySelector('.elements__like');
+    buttonTouchLike.addEventListener("click", function (evt) {
+      evt.target.classList.toggle("elements__like_active");
+    });
 
     const buttonRemove = elementTemplate.querySelector('.elements__remove');
     buttonRemove.addEventListener("click", function removeElementList(evt){
       evt.target.closest('.elements__element').remove();
     });
+
+    const popupImages = () => {
+      popupImage.classList.toggle('popup-images_opened');
+    }
+
+    const elementTemplateImage = elementTemplate.querySelector('.elements__image');
+    elementTemplateImage.addEventListener("click", popupImages);
     
     return elementTemplate;
 };
@@ -49,7 +63,7 @@ const renderList = () => {
     getElement(item));
 
     elementsList.append(...items);
-}
+};
 
 const addElementList = (evt) => {
     evt.preventDefault(); 
@@ -60,15 +74,18 @@ const addElementList = (evt) => {
     stackName.value='';
     stackLink.value='';
     popupAddToggleClose();
-}
+};
 
 formElementAdd.addEventListener('submit', addElementList);
 
 renderList();
 
-// const removeElementList = (evt) => {
-//   evt.target.closest('.elements__element').remove();
-// }
+const popupImageToggleClose = () => {                                          // переписать все тоглы-закрытия на одну функцию
+  popupImage.classList.toggle("popup-images_opened");
+};
+
+buttonClosePopupImage.addEventListener("click",popupImageToggleClose);
+
 
 
 
