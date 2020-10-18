8f3b3dca806b79b976e3bf2b368c9ea7e1e4245c
Dmitry Karyakin <web4t@yandex.ru>
2020-10-18 22:49:41 +0300

    feat: сделать плавность всплытия поп_апов

--- scripts/scriptDev.js
+++ scripts/scriptDev.js
@@ -1,9 +1,9 @@
 const initialCards = [
-  { name: 'Эльбрус', link: '../images/element-grid-elbrus.png'},
-  { name: 'Домбай', link: '../images/element-grid-dombay.png'},
-  { name: 'Карачаева-Черкессия', link: '../images/element-grid-karachaevsk1.jpg'},
-  { name: 'Камчатка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'},
-  { name: 'Холмогорский район',link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'},
+  { name: 'Крым', link: 'https://images.unsplash.com/photo-1599758417353-3b66f35e5d79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'},
+  { name: 'Алтай', link: 'https://images.unsplash.com/photo-1598394188724-cdeb618eb4e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=708&q=80'},
+  { name: 'Карелия', link: 'https://images.unsplash.com/photo-1573156667788-3b0a869a97b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'},
+  { name: 'Санкт-Петербург', link: 'https://images.unsplash.com/photo-1597533849860-5a04a21a7b3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'},
+  { name: 'Дагестан',link: 'https://images.unsplash.com/photo-1591635765226-a7e6533655d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80'},
   { name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'}
 ]; 
 
@@ -24,6 +24,8 @@ const popupAdd = document.querySelector(".popup-add");
 
 const popupAddToggleClose = () => {
     popupAdd.classList.toggle("popup-add_opened");
+    stackName.value='';
+    stackLink.value='';
   };
   
   const popupAddToggleOpen = () => {
@@ -74,9 +76,6 @@ const addElementList = (evt) => {
     const stack = getElement({name: stackName.value, link: stackLink.value});
 
     elementsList.prepend(stack);
-
-    stackName.value='';
-    stackLink.value='';
     popupAddToggleClose();
 };
 
